45eed036da6f0b418597746334cace1c8afbd9cb
tnsthao94 <167952480+SotaThao@users.noreply.github.com>
2026-07-29 08:23:14 +0700

    fix: explicit dark mode class toggle to fix contrast issues in light mode for elements using dark: utilities

--- src/App.tsx
+++ src/App.tsx
@@ -60,8 +60,10 @@ function App() {
   useEffect(() => {
     if (isLightMode) {
       document.body.classList.add('light-theme');
+      document.body.classList.remove('dark-theme');
     } else {
       document.body.classList.remove('light-theme');
+      document.body.classList.add('dark-theme');
     }
   }, [isLightMode]);
 
